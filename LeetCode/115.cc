c2e09fb42c8f28a1e81a2bd3aa905c8cf70040cc
William <williamjy1018@gmail.com>
2021-12-19 11:44:59 +1100

    add 115

---
+++ LeetCode/115.cc
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// double pointers
+class Solution {
+public:
+  int numDistinct(string s, string t) {
+    int s_size = s.size(), t_size = t.size();
+    vector<int> memo(s_size+1,1);
+    for (int i = 0; i < t_size; i++) {
+      int prev = memo[0];
+      int tmp = 0;
+      memo[0] = 0;
+      for (int j = 0; j < s_size; j++) {
+        if (t[i] == s[j]) {
+          tmp = memo[j+1];
+          memo[j+1] = prev + memo[j];
+          prev  = tmp;
+        } else {
+          prev = memo[j+1];
+          memo[j+1] = memo[j];
+        }
+      }
+    }
+    return memo[s_size];
+  }
+};
+
+int main () {
+  Solution solution;
+  string s = "rabbbit";
+  string t = "rabbit";
+  cout << solution.numDistinct(s,t) << endl;
+}
