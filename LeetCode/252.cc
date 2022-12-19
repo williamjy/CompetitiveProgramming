3352103826c9d43b262e3fd39e9d77fb130916ff
williamjy <williamjy1018@gmail.com>
2022-12-20 01:12:54 +1100

    add 252

---
+++ LeetCode/252.cc
@@ -0,0 +1,21 @@
+\#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return true;
+        }
+        sort(intervals.begin(), intervals.end());
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals[i][1] > intervals[i+1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
